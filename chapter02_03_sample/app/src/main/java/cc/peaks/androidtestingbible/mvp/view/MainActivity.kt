4b6a58059c80cd01e7e7087a97e87934cf14cfce
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2018-09-26 12:07:12 -0700

    Implement RecyclerView in chap. 02-03's sample code

--- chapter02_03_sample/app/src/main/java/cc/peaks/androidtestingbible/mvp/view/MainActivity.kt
+++ chapter02_03_sample/app/src/main/java/cc/peaks/androidtestingbible/mvp/view/MainActivity.kt
@@ -2,18 +2,29 @@ package cc.peaks.androidtestingbible.mvp.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import cc.peaks.androidtestingbible.R
 import cc.peaks.androidtestingbible.mvp.model.GitHubRepository
 import cc.peaks.androidtestingbible.mvp.model.LocalDataSource
 import cc.peaks.androidtestingbible.mvp.presenter.ListPresenter
 import cc.peaks.androidtestingbible.retrofit_okhttp.Repo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View {
+  private lateinit var adapter: Adapter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    adapter = Adapter(this)
+
+    recyclerView.apply {
+      setHasFixedSize(true)
+      layoutManager = LinearLayoutManager(this@MainActivity)
+      adapter = this@MainActivity.adapter
+    }
 
     val repository = GitHubRepository(LocalDataSource())
     val presenter = ListPresenter(this, repository)
@@ -22,5 +33,6 @@ class MainActivity : AppCompatActivity(), View {
 
   override fun showRepositoryList(list: List<Repo>) {
     Log.i("MainActivity", list.toString())
+    adapter.updateList(list)
   }
 }
